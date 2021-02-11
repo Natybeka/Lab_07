09c87c3b97c159ff75f260a8dacf98f87f621979
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-02-12 00:23:06 +0300

    Spinner Added when loading from the API

--- asset/js/loadPostsAPI.js
+++ asset/js/loadPostsAPI.js
@@ -1,8 +1,10 @@
 // UI Vars 
 const postDiv3 = document.getElementById('thePosts');
+const spinner = document.querySelector('#spinner');
 
 //Load Every thing ....
 document.addEventListener("DOMContentLoaded", () => {
+  
     load_fromPlaceHolder_new();
     //loadDataNew();
 });
@@ -15,24 +17,28 @@ function load_fromPlaceHolder() {
 }
 //async await
 function load_fromPlaceHolder_new() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-    .then(function(response){return response.json();})
-    .then(function(posts) {
-        let output = ""
-        posts.forEach(function(post) {
-            output += `
-            <div class="item">
-                <div class="img"><img src = "https://images.unsplash.com/photo-1499482125586-91609c0b5fd4?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80" style="width:476px;border-radius:50px;"></div>
-                <div class="content" style="margin:10px;font-size:1.1rem;flex:1">
-                    <a class="header" href="#" id="bTitle">${post.title}</a>
-                    <div class="description"><p id="bDesc">${post.body}</p></div>
-                    <div class="extra"><a class="ui floated basic violet button" href="#">Read More</a></div>
-                </div>
-            </div> 
-            `;
-        })
-        postDiv3.innerHTML = output;
-    }).catch(function(error){console.log(error)})
+            
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(function(response){return response.json();})
+        .then(function(posts) {
+            let output = ""
+            posts.forEach(function(post) {
+                output += `
+                <div class="item post">
+                    <div class="img"><img src = "https://picsum.photos/0/?random=1" style="width:476px;border-radius:50px;"></div>
+                    <div class="content" style="margin:10px;font-size:1.1rem;flex:1">
+                        <a class="header" href="#" id="bTitle">${post.title}</a>
+                        <div class="description"><p id="bDesc">${post.body}</p></div>
+                        <div class="extra"><a class="ui floated basic violet button" href="#">Read More</a></div>
+                    </div>
+                </div> 
+                `;
+            })
+            postDiv3.innerHTML = output;
+        }).catch(function(error){console.log(error)})
+        .finally(function(){spinner.style.display="none"});
+    
+
 
 
 }
