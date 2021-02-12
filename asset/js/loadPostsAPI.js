e0e8b6999d9c98773e011eb657ad126487dd1917
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-02-13 00:11:03 +0300

    Style for clock added

--- asset/js/loadPostsAPI.js
+++ asset/js/loadPostsAPI.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", () => {
     load_fromPlaceHolder_new();
     //loadDataNew();
 });
+async function delaySpinner() {
+    console.log("Code Here")
+    return new Promise((resolve,reject) => {
+        setTimeout(function() {
+            alert("success")
+        },2000)
+    });
+}
 
 
 //load post from fake api function 
@@ -16,27 +24,29 @@ function load_fromPlaceHolder() {
 
 }
 //async await
-function load_fromPlaceHolder_new() {
-            
-        fetch("https://jsonplaceholder.typicode.com/posts")
-        .then(function(response){return response.json();})
-        .then(function(posts) {
-            let output = ""
-            posts.forEach(function(post) {
-                output += `
-                <div class="item post">
-                    <div class="img"><img src = "https://picsum.photos/0/?random=1" style="width:476px;border-radius:50px;"></div>
-                    <div class="content" style="margin:10px;font-size:1.1rem;flex:1">
-                        <a class="header" href="#" id="bTitle">${post.title}</a>
-                        <div class="description"><p id="bDesc">${post.body}</p></div>
-                        <div class="extra"><a class="ui floated basic violet button" href="#">Read More</a></div>
-                    </div>
-                </div> 
-                `;
-            })
-            postDiv3.innerHTML = output;
-        }).catch(function(error){console.log(error)})
-        .finally(function(){spinner.style.display="none"});
+async function load_fromPlaceHolder_new() {
+    await delaySpinner().
+    then (fetch("https://jsonplaceholder.typicode.com/posts")
+            .then(function(response){return response.json();})
+            .then(function(posts) {
+                let output = ""
+                posts.forEach(function(post) {
+                    output += `
+                    <div class="item post">
+                        <div class="img"><img src = "https://picsum.photos/0/?random=1" style="width:476px;border-radius:50px;"></div>
+                        <div class="content" style="margin:10px;font-size:1.1rem;flex:1">
+                            <a class="header" href="#" id="bTitle">${post.title}</a>
+                            <div class="description"><p id="bDesc">${post.body}</p></div>
+                            <div class="extra"><a class="ui floated basic violet button" href="#">Read More</a></div>
+                        </div>
+                    </div> 
+                    `;
+                })
+                postDiv3.innerHTML = output;
+            }).catch(function(error){console.log(error)})
+            .finally(function(){spinner.style.display="none"}))
+        
+        
     
 
 
