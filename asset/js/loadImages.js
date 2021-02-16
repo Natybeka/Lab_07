701069a26055af24ef8fe4fda5f1dfd7b29751c5
Natnael Bekabtu <se.natnael.bekabtu@gmail.com>
2021-02-16 12:22:57 +0300

    Photo Gallery bugs fixed

--- asset/js/loadImages.js
+++ asset/js/loadImages.js
@@ -1,6 +1,6 @@
 loadPhotos()
-const gallery = document.querySelector("#slider-container");
-const images = document.querySelectorAll(".slide-container img");
+const gallery = document.querySelector(".slider-container");
+let images ;  
 
 const prev = document.querySelector("#previous");
 const next = document.querySelector("#next");
@@ -8,32 +8,51 @@ const next = document.querySelector("#next");
 //load the photos
 
 // index and slide width
-let counter = 1;
-const widthSlide = 400;
+let counter = 0;
+let widthSlide = 500;
 
-next.addEventListener('click', () => {
-    gallery.style.trasition="transform 0.5s ease";
-    counter++;
-    gallery.style.transform="translateX(" + (-widthSlide * counter) + "px";
-});
+next.addEventListener('click', slideNext);
+prev.addEventListener('click', slidePrevious);
 
-prev.addEventListener('click', () => {
-    gallery.style.trasition="transform 0.5s ease";
+
+function slidePrevious(e){
+    e.preventDefault();
+    if (counter <= 0) {
+        counter = 9;
+        gallery.style.transform="translateX(" + (-widthSlide * counter) + "px)";
+        return
+    }
+    gallery.style.transition="all 0.4s ease-in-out";
     counter--;
-    gallery.style.transform="translateX(" + (widthSlide * counter) + "px";
-});
+    gallery.style.transform="translateX(" + (-widthSlide * counter) + "px)";
+}
+
+function slideNext(e) {
+    e.preventDefault()
+    console.log("next called");
+    if (counter >= 9) {
+        counter = 0
+        gallery.style.transform="translateX(" + (-widthSlide * counter) + "px)";
+        return;
+    }
+    gallery.style.trasition="all 10s ease-in-out";
+    counter++;
+    gallery.style.transform="translateX(" + (-widthSlide * counter) + "px)";
+}
 
 
 // gallery.style.transform = "translateX(" + (widthSlide * counter) + "px";
 // Loads 30 photos from lorem picsum api
 
 function loadPhotos() {
-    fetch("https://picsum.photos/v2/list")
-    .then(x => {return x.json();})
-    .then(x => {x.forEach(photo => {
-        console.log(photo.width)
-        gallery.innerHTML += `
-            <img src="${photo.download_url}" alt="">
-        `
-    });})
+
+    for(let i = 0; i < 10; i++) {
+        fetch(`https://picsum.photos/500/500?random=${i}`)
+        .then(x => {
+            
+                gallery.innerHTML += `
+            <img src="${x.url}" alt="">
+            `
+        });
+    }
 }
